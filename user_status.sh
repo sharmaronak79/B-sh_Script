f5f03720e2eb6c593b48255938238552b9445a33
Darshanmistry1995 <mistrydarshan1995@gamil.com>
2019-08-11 00:29:22 -0400

    check_user

---
+++ user_status.sh
@@ -0,0 +1,13 @@
+!/bin/bash
+
+echo "Enter username please."
+read username
+echo "Please Enter password"
+read password
+
+if [[ ( $username == "admin" && $password == "admin" ) ]]; then
+
+echo "You  are a valid user";
+else
+echo "Sorry you are not a invalid user"
+fi
